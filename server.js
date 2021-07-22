051d86534d20d6a38585096e6bb77cc287583a4e
kamal-gith <kamaldeenidreez@gmail.com>
2021-07-21 22:36:51 -0700

    added changes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb+srv://kamalai:o3KJeuK9w2FWIyWX@cluster0.mu2f9.mongodb.net/VolunteerDB?authSource=admin&replicaSet=atlas-9dyp7m-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(7000)
