a471084023a474890f25dbd05466030d4f821b8a
kamal-gith <kamaldeenidreez@gmail.com>
2021-07-22 01:37:30 -0700

    .

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
+const { PORT } = process.env;
+const port = process.env.PORT || 7000;
 
 mongoose.connect('mongodb+srv://kamalai:o3KJeuK9w2FWIyWX@cluster0.mu2f9.mongodb.net/VolunteerDB?authSource=admin&replicaSet=atlas-9dyp7m-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
